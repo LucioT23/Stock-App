0d209236b66710d3829f4a8dc7cb05775959ab08
LucioT23 <104279561+LucioT23@users.noreply.github.com>
2023-02-07 18:21:50 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,45 @@
 import streamlit as st
 import pandas as pd
+from operator import index
+import streamlit as st
+import plotly.express as px
+from pycaret.regression import setup, compare_models, pull, save_model, load_model
+import pandas_profiling
+from streamlit_pandas_profiling import st_profile_report
+import os 
+
 st.title('A Simple Streamlit Tony Web App')
-#name = st.text_input('Enter your name', '')
-#st.write(f'Hello {name}!')
-#x = st.slider(“Select an integer x”, 0, 10, 1)
-#y = st.slider(“Select an integer y”, 0, 10, 1)
-#df = pd.DataFrame({“x”: [x], “y”: [y] , “x + y”: [x + y]}, index = [“addition row”])
-#st.write(df)
+
+with st.sidebar: 
+    st.image("https://www.onepointltd.com/wp-content/uploads/2020/03/inno2.png")
+    st.title("Mobile Client Fleet Check_ML")
+    choice = st.radio("Navigation", ["Upload","Profiling","Modelling", "Download"])
+    st.info("This project application helps you check your customer mobile fleet data.")
+
+if choice == "Upload":
+    st.title("Upload Your Dataset")
+    file = st.file_uploader("Upload Your Dataset")
+    if file: 
+        df = pd.read_csv(file, index_col=None)
+        df.to_csv('dataset.csv', index=None)
+        st.dataframe(df)
+
+if choice == "Profiling": 
+    st.title("Exploratory Data Analysis")
+    profile_df = df.profile_report()
+    st_profile_report(profile_df)
+
+if choice == "Modelling": 
+    chosen_target = st.selectbox('Choose the Target Column', df.columns)
+    if st.button('Run Modelling'): 
+        setup(df, target=chosen_target, silent=True)
+        setup_df = pull()
+        st.dataframe(setup_df)
+        best_model = compare_models()
+        compare_df = pull()
+        st.dataframe(compare_df)
+        save_model(best_model, 'best_model')
+
+if choice == "Download": 
+    with open('best_model.pkl', 'rb') as f: 
+        st.download_button('Download Model', f, file_name="best_model.pkl")
