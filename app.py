ee910c8ebde7f6e0927447e8fd822e55ce3f0e8f
LucioT23 <104279561+LucioT23@users.noreply.github.com>
2023-02-07 16:11:37 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-st.title('A Simple Streamlit Web App')
+st.title('A Simple Streamlit Tony Web App')
 #name = st.text_input('Enter your name', '')
 #st.write(f'Hello {name}!')
 #x = st.slider(“Select an integer x”, 0, 10, 1)
