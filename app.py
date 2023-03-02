c4010d3c0d3fa71b2810dd8e5cc6e9b68f2b09dc
LucioT23 <104279561+LucioT23@users.noreply.github.com>
2023-03-02 19:32:10 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -3,18 +3,11 @@ import pandas as pd
 import numpy as np 
 from datetime import datetime 
 import streamlit as st
-#! pip install plotly_express
 import plotly.express as px
 
 pd.set_option('display.max_row',111)
 pd.set_option('display.max_column',111)
-#import openpyxl
-#from operator import index
-#import pycaret
-#from pycaret.regression import setup, compare_models, pull, save_model, load_model
-#import pandas_profiling
-#from streamlit_pandas_profiling import st_profile_report
-#import os 
+
 
 st.title('A Simple Tony Web App')
 
@@ -30,6 +23,59 @@ if choice == "Download":
     file = st.file_uploader("Download Your File")
     if file: 
         df = pd.read_csv(file, index_col=None)
+        df= df.drop(columns=['application déployée'])
+
+        # Renome les portails avec des noms uniques (EWOCS/GLM AC/MWM)
+        def rename_application(df):
+          for i,val in enumerate(df['Application déployée']):
+            #print(i,val)
+            if val == 'MWM, MWM' or val == 'MWM':
+              df.loc[i,'Portail déployée']='MWM'    
+            elif val == 'EWOCS, EWOCS' or val =='EWOCS':
+              df.loc[i,'Portail déployée']='EWOCS'
+            elif val == 'GLM AC, GLM AC' or val == 'GLM AC, MWM' or val == 'GLM AC, EWOCS' or val =='GLM AC' or val =='HUG':
+              df.loc[i,'Portail déployée']='GLM AC'
+          return df
+
+        df= rename_application(df)
+
+        # tranfo en date toutes les colonnes avec un format date
+        date_columns = [col for col in df.columns if 'date' in col]
+        for col in date_columns :
+          df[col]= pd.to_datetime(df[col],format='%Y %m %d')
+
+        today = datetime.now().date()
+        today = pd.to_datetime(today,format='%Y %m %d')
+
+        # MAJ du statut de déploiement
+        def statut(row):
+            if pd.notnull(row['date Vie de Solution']) and row['date Vie de Solution'].date() < today:
+                return 'Déployé'
+            elif pd.notnull(row['date Kick-off Interne']) and pd.isnull(row['date Vie de Solution']):
+                if row['Statut'] == 'Stand by':
+                    return 'Non déployé'
+                else:
+                    return 'En cours'
+            else:
+                return 'Non déployé'
+
+        df['statut deploiement'] = df.apply(statut, axis=1)
+
+        # select not renouvellement ou avenant
+        df = df[df['Renouvellement'].isna()]
+        #df = df[df['Portail déployée']!='GLM AC]]['Renouvellement'].isna()
+
+        # replace NaN by 0
+        df["Code groupe DISE"] = df["Code groupe DISE"].fillna("0")
+
+        # Replace ',' '/' and '-' with ';'
+        df["Code groupe DISE"] = df["Code groupe DISE"].str.replace(r'[,/-]', ';')
+        df["Code groupe DISE"] = df["Code groupe DISE"].astype(str).apply(lambda x: [int(i) for i in x.split(';')])
+
+        df['quarter'] = pd.PeriodIndex(df['date Kick-off Interne'], freq='Q')
+        df['quarterc'] = df['quarter'].astype('string')
+
+        # sauvegarde + affichage
         df.to_csv('dataset.csv', index=None)
         st.dataframe(df)
 
