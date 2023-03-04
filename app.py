81752e2704e90d4de3d4e5a53695f685dfe383ee
LucioT23 <104279561+LucioT23@users.noreply.github.com>
2023-03-04 16:54:43 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -141,11 +141,11 @@ if choice == "GLM AC deployment in progress":
         fig3 = px.histogram(df_deploiement, x="quarterc",color='statut deploiement', title="Statut déploiement par date de Kickoff (GLM AC)")
         fig3.update_layout(height=400,width =800)
 
-        def improve_text_position(x):
-          positions = ['top center','top right', 'bottom center', 'bottom left']  # you can add more: left center ...
-          return [positions[i % len(positions)] for i in range(len(x))]
+        #def improve_text_position(x):
+        #  positions = ['top center','top right', 'bottom center', 'bottom left']  # you can add more: left center ...
+        #  return [positions[i % len(positions)] for i in range(len(x))]
 
-        fig3.update_traces(textposition=improve_text_position(data3["quarterc"]))
+        #fig3.update_traces(textposition=improve_text_position(data3["quarterc"]))
 
         #Ajout Graph Statut déploiement par date de Kickoff (GLM AC)
         st.subheader("Statut déploiement par date de Kickoff (GLM AC)")
