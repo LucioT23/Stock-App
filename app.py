d3d225c280bbf338055635fd5088864a3d68c5ca
LucioT23 <104279561+LucioT23@users.noreply.github.com>
2023-04-06 16:26:06 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -95,8 +95,8 @@ if choice == "Download_2":
     st.header("Download Your Tosca Dataset") # ou st.subheader()
     file_Tosca = st.file_uploader("Download Your File")
     if file_Tosca: 
-        df_Tosca = pd.read_csv(file_Tosca, error_bad_lines=False)
-        df_Tosca.to_csv('dataset_Tosca.csv', sep=';')
+        df_Tosca = pd.read_csv(file_Tosca, sep=';', warn_bad_lines=True,error_bad_lines=False)
+        df_Tosca.to_csv('dataset_Tosca.csv', index=None)
         st.dataframe(df_Tosca)
 
 
