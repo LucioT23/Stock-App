058fffb866f5cc2dd49ec316777c62a96194e493
LucioT23 <104279561+LucioT23@users.noreply.github.com>
2023-04-07 14:50:51 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -21,7 +21,12 @@ def nb_actif(df,df_Tosca):
   actif_par_code = dict(zip(df_Tosca_test['Code groupe DISE'], df_Tosca_test['nb_actif']))
 
   def calculer_actif(codes):
-      codes = [int(c) for c in codes.split(',')]
+      #codes = [int(c) for c in codes.split(',')]
+      if isinstance(codes, str):
+        codes = [int(c) for c in codes.split(',')]
+      else:
+        codes = [int(codes)]
+      #return len(codes)
       return sum(actif_par_code.get(c, 0) for c in codes)
 
   df_test["Nb_actifs"] = df_test['Code groupe DISE'].apply(calculer_actif)
