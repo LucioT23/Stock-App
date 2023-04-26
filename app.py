11734dcf1f37560c9810b9d99342f20c2a656caf
LucioT23 <104279561+LucioT23@users.noreply.github.com>
2023-04-26 14:48:41 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -301,9 +301,10 @@ def client_MWM(df_mwm, df_Planning_data, plus_recente):
   # Obtenir la plus récente valeur de la colonne 'trimestre_digital'
   # plus_recente = data['trimestre_digital'].max()
 
+  st.write(type(plus_recente))
   # clients MWM en déploiement GLM AC
   test = df_mwm[df_mwm['état'] != 'MWM'].copy()
-  test['trimestre_deployable_GLM'] = test['trimestre_deployable_GLM'].fillna('2023Q2') #plus_recente
+  test['trimestre_deployable_GLM'] = test['trimestre_deployable_GLM'].fillna(2023Q2) #plus_recente
   #test['trimestre_deployable_GLM'] = pd.to_datetime(test['trimestre_deployable_GLM'], format='%YQ%q', errors='coerce').dt.to_period('Q').astype(str)
   test['trimestre_deployable_GLM'] = pd.to_datetime(test['trimestre_deployable_GLM']).dt.to_period('Q').astype(str)
   st.write(test)
