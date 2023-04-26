e7596f011885db8b1928faae3626af08d7247c00
LucioT23 <104279561+LucioT23@users.noreply.github.com>
2023-04-26 14:25:41 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -304,7 +304,7 @@ def client_MWM(df_mwm, df_Planning_data, plus_recente):
   # clients MWM en déploiement GLM AC
   test = df_mwm[df_mwm['état'] != 'MWM'].copy()
   test['trimestre_deployable_GLM'] = test['trimestre_deployable_GLM'].fillna(plus_recente)
-  test['trimestre_deployable_GLM'] = pd.to_datetime(test['trimestre_deployable_GLM'], format='%YQ%q').dt.to_period('Q').astype(str)
+  test['trimestre_deployable_GLM'] = pd.to_datetime(test['trimestre_deployable_GLM'], format='%YQ%q', errors='coerce').dt.to_period('Q').astype(str)
   st.write(test['trimestre_deployable_GLM'])
 
   # Ajout des lignes de df2 à df1 en utilisant append()
