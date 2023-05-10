8df814bfe5536c10b205d8fb6a7f610011315a56
LucioT23 <104279561+LucioT23@users.noreply.github.com>
2023-05-10 12:01:21 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -817,8 +817,7 @@ if choice == "Test":
         st.write(df_concat)
 
         fig2 = px.bar(df_concat, x='trimestre_deployable_GLM',
-                      hover_name='title', text='title',color='état'),
-                      category_orders={'trimestre_deployable_GLM': ['2020Q2', '2022Q2', '2022Q3','2022Q4','2023Q1','2023Q2','2023Q3','2023Q4','2024Q1','2024Q2','2024Q3']})
+                      hover_name='title', text='title',color='état'), category_orders={'trimestre_deployable_GLM': ['2020Q2', '2022Q2', '2022Q3','2022Q4','2023Q1','2023Q2','2023Q3','2023Q4','2024Q1','2024Q2','2024Q3']})
         
         fig2.update_layout(height=600,width =1200,xaxis_title="Trimestre (Kick off)",
                           yaxis_title="Nombre de déploiement",
