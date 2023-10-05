6379f8023c191974e60660a59ccd73a3e6fdc662
LucioT23 <104279561+LucioT23@users.noreply.github.com>
2023-10-05 18:30:10 +0200

    Add files via upload

--- immo_app.py
+++ immo_app.py
@@ -18,11 +18,17 @@ st.title(' :house: Immo Data Analyze')
 # Pour remonter le titre dans la page
 st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
 
-fl = st.file_uploader(":file_folder: Upload a file",type=(["csv","txt","xlsx","xls"]))
+fl = st.file_uploader(" :file_folder: Upload a file",type=(["csv","txt","xlsx","xls"]))
 if fl is not None:
     filename = fl.name
     st.write(filename)
     df = pd.read_csv(filename, encoding = "ISO-8859-1")
         
 
-st.write(df)
+st.sidebar.header("Choose your filter: ")
+# Create for Region
+nb_rooms = st.sidebar.multiselect("Nombre de chambre", df["Number Room"].unique())
+if not nb_rooms:
+    df2 = df.copy()
+else:
+    df2 = df[df["Number Room"].isin(Number Room)]
