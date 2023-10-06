af7faa8e3279e893190e67f6aed261526620780f
LucioT23 <104279561+LucioT23@users.noreply.github.com>
2023-10-06 19:25:45 +0200

    Add files via upload

--- immo_app.py
+++ immo_app.py
@@ -81,8 +81,8 @@ fig = px.scatter_mapbox(filtered_df, lat="latitude", lon="longitude", color="eur
 st.plotly_chart(fig,use_container_width=True, height = 500, width = 1000)
 
 with st.expander("Nb_de_biens_ViewData"):
-    nb_rooms = filtered_df.groupby(by = "Number Room", as_index = False)['Title'].count()
-    st.write(nb_rooms.style.background_gradient(cmap="Oranges"))
+    rooms = filtered_df.groupby(by = "Number Room", as_index = False)['Title'].count()
+    st.write(rooms.style.background_gradient(cmap="Blues"))
     csv = region.to_csv(index = False).encode('utf-8')
     st.download_button("Download Data", data = csv, file_name = "Bien par chambre.csv", mime = "text/csv",
                     help = 'Click here to download the data as a CSV file')
