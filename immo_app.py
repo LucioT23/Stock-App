2713ad220005e22072c3e9e7ae868e65e5ef0bc7
LucioT23 <104279561+LucioT23@users.noreply.github.com>
2023-10-20 18:08:12 +0200

    Add files via upload

--- immo_app.py
+++ immo_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib
 from datetime import datetime
 import plotly.express as px
 import base64
