1ba498ecd0bc7a81b924b6086b3fb56c56e9fcca
LucioT23 <104279561+LucioT23@users.noreply.github.com>
2023-10-31 14:27:03 +0100

    Add files via upload

--- stock_app.py
+++ stock_app.py
@@ -18,14 +18,83 @@ st.title(' :house: Stock Prediction')
 # Pour remonter le titre dans la page
 st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
 
-fl = st.file_uploader(" :file_folder: Upload a file",type=(["csv","txt","xlsx","xls"]))
-if fl is not None:
-    filename = fl.name
-    st.write(filename)
-    df = pd.read_csv(filename) #, encoding = "ISO-8859-1")
+# pip install streamlit fbprophet yfinance plotly
+import streamlit as st
+from datetime import date
 
-df['Date']=pd.to_datetime(df['Date'])
+import yfinance as yf
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
+from plotly import graph_objs as go
+
+START = "2015-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
+
+st.title('Stock Forecast App')
+
+stocks = ('GOOG', 'AAPL', 'MSFT', 'GME')
+selected_stock = st.selectbox('Select dataset for prediction', stocks)
+
+n_years = st.slider('Years of prediction:', 1, 4)
+period = n_years * 365
+
+
+@st.cache
+def load_data(ticker):
+    data = yf.download(ticker, START, TODAY)
+    data.reset_index(inplace=True)
+    return data
+
+	
+data_load_state = st.text('Loading data...')
+data = load_data(selected_stock)
+data_load_state.text('Loading data... done!')
+
+st.subheader('Raw data')
 with st.expander("Data"):
-    st.dataframe(df.style.background_gradient(cmap="Oranges"))
+    st.dataframe(data.style.background_gradient(cmap="Oranges"))
 
-st.line_chart(data=df, x=df['Close'], y=df['Date'], use_container_width=True) #color='r'
+# Plot raw data
+def plot_raw_data():
+	fig = go.Figure()
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"), color ="blue")
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"), color = "red")
+	fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
+	st.plotly_chart(fig)
+	
+plot_raw_data()
+
+# Predict forecast with Prophet.
+df_train = data[['Date','Close']]
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)
+
+# Show and plot forecast
+st.subheader('Forecast data')
+with st.expander("Data"):
+    st.dataframe(forecast.style.background_gradient(cmap="Blues"))
+    
+st.write(f'Forecast plot for {n_years} years')
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
+
+st.write("Forecast components")
+fig2 = m.plot_components(forecast)
+st.write(fig2)
+
+#fl = st.file_uploader(" :file_folder: Upload a file",type=(["csv","txt","xlsx","xls"]))
+#if fl is not None:
+#    filename = fl.name
+#    st.write(filename)
+#    df = pd.read_csv(filename) #, encoding = "ISO-8859-1")
+
+#df['Date']=pd.to_datetime(df['Date'])
+
+
+
+#chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+#st.line_chart(data=df, x=df['Close'], y=df['Date']) 
