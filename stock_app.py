66991ac5d1e9a3a294118c9d85896143949b5b0d
LucioT23 <104279561+LucioT23@users.noreply.github.com>
2023-10-31 16:16:42 +0100

    Add files via upload

--- stock_app.py
+++ stock_app.py
@@ -8,16 +8,6 @@ import base64
 import warnings
 warnings.filterwarnings('ignore')
 
-pd.set_option('display.max_row',111)
-pd.set_option('display.max_column',111)
-
-# pour nommer la page
-st.set_page_config(page_title="Stock Prediction!!!", page_icon=":euro:",layout="wide")
-
-st.title(' :house: Stock Prediction')
-# Pour remonter le titre dans la page
-st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
-
 # pip install streamlit fbprophet yfinance plotly
 from datetime import date
 
@@ -26,13 +16,23 @@ from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go
 
+pd.set_option('display.max_row',111)
+pd.set_option('display.max_column',111)
+
+# pour nommer la page
+st.set_page_config(page_title="Stock Prediction !!!", page_icon=":euro:",layout="wide")
+
+st.title(' :euro: Prédiction des cours de l'action')
+# Pour remonter le titre dans la page
+st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
+
 START = "2015-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
 stocks = ('DG.PA', 'ORA.PA', 'AAPL', 'GOOG')
-selected_stock = st.selectbox('Select dataset for prediction', stocks)
+selected_stock = st.selectbox('Selectionner l'action pour la prédiction', stocks)
 
-n_months = st.slider('Months of prediction:', 1, 12)
+n_months = st.slider('Nombre de mois à prédire:', 1, 12)
 #n_years = st.slider('Years of prediction:', 1, 4)
 #period = n_years * 365
 period = n_months * 30
@@ -43,12 +43,12 @@ def load_data(ticker):
     data.reset_index(inplace=True)
     return data
 	
-data_load_state = st.text('Loading data...')
+data_load_state = st.text('Chargement des données...')
 data = load_data(selected_stock)
-data_load_state.text('Loading data... done!')
+data_load_state.text('Chargement des données... prêt!')
 
-st.subheader('Raw data')
-with st.expander("Data"):
+st.subheader('Données extraites')
+with st.expander("Données"):
     st.dataframe(data.style.background_gradient(cmap="Oranges"))
 
 # Plot raw data
@@ -56,7 +56,7 @@ def plot_raw_data():
 	fig = go.Figure()
 	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
 	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
-	fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
+	fig.layout.update(title_text='Evolution du cours de l'action', xaxis_rangeslider_visible=True)
 	st.plotly_chart(fig)
 	
 plot_raw_data()
@@ -71,17 +71,17 @@ future = m.make_future_dataframe(periods=period)
 forecast = m.predict(future)
 
 # Show and plot forecast
-st.subheader('Forecast data')
-with st.expander("Data"):
+st.subheader('Prédictions')
+with st.expander("Données prédites"):
     st.dataframe(forecast.style.background_gradient(cmap="Blues"))
     
-st.write(f'Forecast plot for {n_months} months')
+st.write(f'Prédictions pour {n_months} mois')
 fig1 = plot_plotly(m, forecast)
 fig1.update_layout(yaxis_title="Cours de l'action €", xaxis_title = "Date")
 st.plotly_chart(fig1,use_container_width=True)
 #st.plotly_chart(fig1)
 
-st.write("Forecast components")
+st.write("Variables de prédictions")
 fig2 = m.plot_components(forecast)
 st.write(fig2)
 
